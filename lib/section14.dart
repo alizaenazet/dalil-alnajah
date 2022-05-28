329a53e973837e9d09a48b953eb31b1a8ffa4fe7
Qori El-Hafizh <contact@qori.dev>
2022-05-28 10:10:33 +0700

    section 14 fonts

--- lib/section14.dart
+++ lib/section14.dart
@@ -5,195 +5,235 @@ import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'config/consts.dart';
+
 class Section14 extends StatefulWidget {
-  const Section14({ Key? key }) : super(key: key);
+  const Section14({Key? key}) : super(key: key);
 
   @override
   State<Section14> createState() => _Section14State();
 }
 
 class _Section14State extends State<Section14> {
-  final Color color=Colors.white;
-   double size=32;
+  final Color color = Colors.white;
+  double size = 32;
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      
-       backgroundColor: Colors.white,
-       appBar: AppBar(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: IconButton(onPressed: (){
-         Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>Home()));
-        }, icon:Icon(Icons.arrow_back,color: Colors.black,size: 25,)
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => Home()));
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 25,
+            )),
+        title: Text(
+          'Sayyidul istighfar',
+          style: GoogleFonts.poppins(
+              textStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
         ),
-        title: Text('Sayyidul istighfar',style: GoogleFonts.poppins(
-          textStyle:TextStyle(
-            color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold
-          )
-        ),),
-      
       ),
-       body: Stack(
-         children: [SingleChildScrollView(
+      body: Stack(children: [
+        SingleChildScrollView(
           child: Column(
             children: [
-                SizedBox(height: 40,),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            child: Container(
-                        height: 50,
-                        color: color,
-                        width: MediaQuery.of(context).size.width ,
-                        child: Text('بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ',
-                        textDirection: TextDirection.rtl,
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontSize: size,
-                            fontWeight: FontWeight.bold
-                        ),
-                        ),
-                      ),
-                          ),
-                       
-                     
-                    SizedBox(height: 20,),
-                      Padding(
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  height: 50,
+                  color: color,
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ',
+                    textDirection: TextDirection.rtl,
+                    style:
+                        largeGreenArabic.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Container(
                   height: 400,
                   width: MediaQuery.of(context).size.width,
                   color: Colors.white,
-                  child: Text('للَّهُمَّ أَنْتَ رَبِّيْ لاَ إِلَـهَ إِلاَّ أَنْتَ، خَلَقْتَنِيْ وَأَنَا عَبْدُكَ'
-                  +' ،'+'وَأَنَا عَلَى عَهْدِكَ وَوَعْدِكَ مَا اسْتَطَعْتُ'
-                  +' ،'+'أَعُوْذُ بِكَ مِنْ شَرِّ مَا صَنَعْتُ، أَبُوْءُ لَكَ بِنِعْمَتِكَ عَلَيَّ'
-                  +'  '+'وَأَبُوْءُ بِذَنْبِيْ فَاغْفِرْ لِيْ فَإِنَّهُ لاَ يَغْفِرُ'
-                  +'  '+'الذُّنُوْبَ إِلاَّ أَنْتَ'
-                  
-                 
-                  ,
-                  textDirection: TextDirection.rtl,
-                  style: GoogleFonts.poppins(
-                    textStyle:TextStyle(
-                      color:Colors.black,
-                     fontSize: size
-                    )
-                  ),),
+                  child: Text(
+                    'للَّهُمَّ أَنْتَ رَبِّيْ لاَ إِلَـهَ إِلاَّ أَنْتَ، خَلَقْتَنِيْ وَأَنَا عَبْدُكَ' +
+                        ' ،' +
+                        'وَأَنَا عَلَى عَهْدِكَ وَوَعْدِكَ مَا اسْتَطَعْتُ' +
+                        ' ،' +
+                        'أَعُوْذُ بِكَ مِنْ شَرِّ مَا صَنَعْتُ، أَبُوْءُ لَكَ بِنِعْمَتِكَ عَلَيَّ' +
+                        '  ' +
+                        'وَأَبُوْءُ بِذَنْبِيْ فَاغْفِرْ لِيْ فَإِنَّهُ لاَ يَغْفِرُ' +
+                        '  ' +
+                        'الذُّنُوْبَ إِلاَّ أَنْتَ',
+                    textDirection: TextDirection.rtl,
+                    style: largeBlackArabic,
+                  ),
                 ),
               ),
-            
-           
-          SizedBox(height: 50,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.star,color:Colors.green,size: 15,),
-                      Icon(Icons.star,color:Colors.green,size: 15,),
-                        Icon(Icons.star,color:Colors.green,size: 15,),
-                          Icon(Icons.star,color:Colors.green,size: 15,),
-                            Icon(Icons.star,color:Colors.green,size: 15,),
-                  ],
-                ),
-                 SizedBox(height: 50,),
-          
-           
-            
-       
-            ],
-          ),
-             ),
-               Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              height: 60,
-              width:MediaQuery.of(context).size.width,
-              
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-  height: 55,
-  width: 55,
-  decoration: BoxDecoration(
-    color: Colors.white,
-    shape: BoxShape.circle,
-    boxShadow: [BoxShadow(
-      color: Colors.grey.shade300,
-      spreadRadius: 4,blurRadius: 4
-    )]
-  ),
-  child: Center(
-    child: IconButton(onPressed: (){
-
-    Navigator.of(context).pushReplacement(PageRouteBuilder(
-                              transitionDuration: Duration(seconds: 1),
-                              transitionsBuilder: (BuildContext context,
-                                  Animation<double> animation,
-                                  Animation<double> secAnimation,
-                                  Widget child) {
-                                animation = CurvedAnimation(
-                                    parent: animation, curve: Curves.linear);
-                                return SharedAxisTransition(
-                                    child: child,
-                                    animation: animation,
-                                    secondaryAnimation: secAnimation,
-                                    transitionType: SharedAxisTransitionType.horizontal);
-                              },
-                              pageBuilder: (BuildContext context,
-                                  Animation<double> animation,
-                                  Animation<double> secAnimation) {
-                                return Section13();
-                              }));
-    }, icon: Icon(Icons.arrow_back_ios,color: Colors.green,size: 20,)
-    ),
-  ),
-),
-                  
-Container(
-  height: 55,
-  width: 55,
-  decoration: BoxDecoration(
-    color: Colors.white,
-    shape: BoxShape.circle,
-    boxShadow: [BoxShadow(
-      color: Colors.grey.shade300,
-      spreadRadius: 4,blurRadius: 4
-    )]
-  ),
-  child: IconButton(onPressed: (){
-
-  Navigator.of(context).pushReplacement(PageRouteBuilder(
-                            transitionDuration: Duration(seconds: 1),
-                            transitionsBuilder: (BuildContext context,
-                                Animation<double> animation,
-                                Animation<double> secAnimation,
-                                Widget child) {
-                              animation = CurvedAnimation(
-                                  parent: animation, curve: Curves.linear);
-                              return SharedAxisTransition(
-                                  child: child,
-                                  animation: animation,
-                                  secondaryAnimation: secAnimation,
-                                  transitionType: SharedAxisTransitionType.horizontal);
-                            },
-                            pageBuilder: (BuildContext context,
-                                Animation<double> animation,
-                                Animation<double> secAnimation) {
-                              return Section15();
-                            }));
-  }, icon: Icon(Icons.arrow_forward_ios,color: Colors.green,size: 20,)
-  ),
-),
+                  Icon(
+                    Icons.star,
+                    color: Colors.green,
+                    size: 15,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.green,
+                    size: 15,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.green,
+                    size: 15,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.green,
+                    size: 15,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.green,
+                    size: 15,
+                  ),
                 ],
               ),
-            ),
-
-          ],
-      ),
-        )  
-      ] ),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 55,
+                      width: 55,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.shade300,
+                                spreadRadius: 4,
+                                blurRadius: 4)
+                          ]),
+                      child: Center(
+                        child: IconButton(
+                            onPressed: () {
+                              Navigator.of(context).pushReplacement(
+                                  PageRouteBuilder(
+                                      transitionDuration: Duration(seconds: 1),
+                                      transitionsBuilder: (BuildContext context,
+                                          Animation<double> animation,
+                                          Animation<double> secAnimation,
+                                          Widget child) {
+                                        animation = CurvedAnimation(
+                                            parent: animation,
+                                            curve: Curves.linear);
+                                        return SharedAxisTransition(
+                                            child: child,
+                                            animation: animation,
+                                            secondaryAnimation: secAnimation,
+                                            transitionType:
+                                                SharedAxisTransitionType
+                                                    .horizontal);
+                                      },
+                                      pageBuilder: (BuildContext context,
+                                          Animation<double> animation,
+                                          Animation<double> secAnimation) {
+                                        return Section13();
+                                      }));
+                            },
+                            icon: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.green,
+                              size: 20,
+                            )),
+                      ),
+                    ),
+                    Container(
+                      height: 55,
+                      width: 55,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.shade300,
+                                spreadRadius: 4,
+                                blurRadius: 4)
+                          ]),
+                      child: IconButton(
+                          onPressed: () {
+                            Navigator.of(context).pushReplacement(
+                                PageRouteBuilder(
+                                    transitionDuration: Duration(seconds: 1),
+                                    transitionsBuilder: (BuildContext context,
+                                        Animation<double> animation,
+                                        Animation<double> secAnimation,
+                                        Widget child) {
+                                      animation = CurvedAnimation(
+                                          parent: animation,
+                                          curve: Curves.linear);
+                                      return SharedAxisTransition(
+                                          child: child,
+                                          animation: animation,
+                                          secondaryAnimation: secAnimation,
+                                          transitionType:
+                                              SharedAxisTransitionType
+                                                  .horizontal);
+                                    },
+                                    pageBuilder: (BuildContext context,
+                                        Animation<double> animation,
+                                        Animation<double> secAnimation) {
+                                      return Section15();
+                                    }));
+                          },
+                          icon: Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.green,
+                            size: 20,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
